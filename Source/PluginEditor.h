74891ab0dc665df87066c1c6694af35848918339
aik2mlj <aik2mlj@gmail.com>
2021-10-30 13:51:10 +0800

    [Editor]: Added sliders to GUI

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,23 +11,40 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider {
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                        juce::Slider::TextEntryBoxPosition::NoTextBox) {
+
+    }
+};
+
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
-{
+class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor {
 public:
-    SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
+    SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor &);
+
     ~SimpleEQAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics &) override;
+
     void resized() override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
-    SimpleEQAudioProcessor& audioProcessor;
+    SimpleEQAudioProcessor &audioProcessor;
+
+    CustomRotarySlider peakFreqSlider,
+            peakGainSlider,
+            peakQualitySlider,
+            lowCutFreqSilder,
+            highCutFreqSlider,
+            lowCutSlopeSlider,
+            highCutSlopeSlider;
+    std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
