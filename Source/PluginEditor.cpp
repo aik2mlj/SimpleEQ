9018db7fc2f5a49ce30eb15f06fc3ddfbe1cda0d
aik2mlj <aik2mlj@gmail.com>
2021-11-02 12:49:25 +0800

    [Editor]: sliders are now circles

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -49,11 +49,17 @@ void RotarySliderWithLabels::paint(juce::Graphics &g) {
     using namespace juce;
     // 7'o clock to 5'o clock
     auto startAng = degreesToRadians(180.f + 45.f);
-    auto endAng = degreesToRadians(180.f - 25.f) + MathConstants<float>::twoPi;
+    auto endAng = degreesToRadians(180.f - 45.f) + MathConstants<float>::twoPi;
 
     auto range = getRange();
 
     auto sliderBounds = getSliderBounds();
+
+    g.setColour(Colours::red);
+    g.drawRect(getLocalBounds());
+    g.setColour(Colours::yellow);
+    g.drawRect(sliderBounds);
+
     getLookAndFeel().drawRotarySlider(g,
                                       sliderBounds.getX(), sliderBounds.getY(),
                                       sliderBounds.getWidth(), sliderBounds.getHeight(),
@@ -63,7 +69,15 @@ void RotarySliderWithLabels::paint(juce::Graphics &g) {
 }
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const {
-    return getLocalBounds();
+    // resize into a smaller square
+    auto bounds = getLocalBounds();
+    auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
+    size -= getTextHeight() * 2;
+    juce::Rectangle<int> r;
+    r.setSize(size, size);
+    r.setCentre(bounds.getCentreX(), 0);
+    r.setY(2); // a little gap to the top
+    return r;
 }
 
 //==============================================================================
